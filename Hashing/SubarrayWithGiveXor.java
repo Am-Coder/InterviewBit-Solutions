33bce3f030181c3821960c955c7c2bad1796840a
Am-Coder <mishraaman2210@gmail.com>
2020-10-25 17:32:42 +0530

    Hashing Problems

---
+++ Hashing/SubarrayWithGiveXor.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solve(int[] A, int B) {
+        int n = A.length;
+        int[] pref = new int[n+1];
+        for(int i=1; i<=n; i++){
+            pref[i] = A[i-1]^pref[i-1];
+        }   
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int cou = 0;
+        for(int i=0; i<=n; i++){
+            if(hm.containsKey(pref[i]^B)){
+                cou += hm.get(pref[i]^B);
+            }
+            hm.put(pref[i], hm.getOrDefault(pref[i],0)+1);
+        }
+        return cou;
+    }       
+}           
+            
